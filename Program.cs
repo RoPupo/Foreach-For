78e616bc1f842124b7d63a8d9253ff44892be1f6
Rodrigo <Rodrigo@RODRIGOPUPO>
2023-02-10 10:22:03 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Metodo For :");
+
+string[] sobrenome = { "P", "PU", "PUP", "PUPO" };
+for (int i = 0; i < sobrenome.Length; ++i)
+{
+    Console.WriteLine(sobrenome[i]);
+}
+ // Mesmo resultado, utilizando menos comandos
+Console.WriteLine("");
+Console.WriteLine( "Metodo Foreach :");
+
+string[] rodrigo = { "R", "RO", "ROD", "RODR", "RODRI", "RODRIG", "RODRIGO" };
+foreach (string nome in rodrigo)
+{
+    Console.WriteLine(nome);
+}
